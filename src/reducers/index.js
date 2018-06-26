3145ec2938a2cad46ca93fb07682c111dfcb6a7c
thangn <thangn.1411@gmail.com>
2018-06-26 13:40:08 +0700

    rm file unused

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,5 @@
 
 import { combineReducers } from 'redux'
-import generateWallet from './generateWallet'
 import notification from './notification';
 import wallet from './wallet';
 import assets from './assets';
@@ -10,7 +9,6 @@ import asset from './asset';
 import history from './history';
 import unlockDialog from './unlockdialog';
 const rootReducer = combineReducers({
-  generateWallet,
   notification,
   wallet,
   assets,
